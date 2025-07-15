0e7d75a72beccfa8a465c024b309fb4f36434aff
Dilip9090 <137048001+Dilip9090@users.noreply.github.com>
2025-07-15 11:00:48 +0530

    Add files via upload

---
+++ Day 5/list.py
@@ -0,0 +1,11 @@
+marks = [45.67 , 87.87 , 49.58 , 78.87 , 37.48]
+
+print(marks)
+print(type(marks))
+
+print(marks[0])
+
+marks[0] = "Alia"
+
+print(marks)
+print(marks[1:3])
---
+++ Day 5/list_method.py
@@ -0,0 +1,19 @@
+list = [3,2,1,4,5]
+
+list.append(6)
+
+print(list)
+
+list.sort()
+print(list)
+list.sort(reverse = True)
+print(list)
+
+list.reverse()
+print(list)
+
+list.insert(1,9)
+print(list)
+
+list.remove(1)
+print(list)
